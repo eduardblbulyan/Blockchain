feebbfb5f8021d3c5179a819807ae08c8a54a052
Edward <edw.blb@gmail.com>
2023-12-17 16:19:08 +0400

    Define blockchain operations

--- main.py
+++ main.py
@@ -12,3 +12,16 @@ class Blockchain:
         self.transactions = []
         self.create_block(proof=1, previous_hash='0')
         self.nodes = set()
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain)+1,
+            'timestamp': time.time(),
+            'proof': proof,
+            'previous_hash': previous_hash,
+            'transactions': self.transactions
+        }
+        # Reset for the next block
+        self.transactions = []
+        self.chain.append(block)
+        return block
