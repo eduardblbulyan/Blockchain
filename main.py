946862abab10b5b7df05da2f384afe20f081e244
Edward <edw.blb@gmail.com>
2023-12-17 16:15:08 +0400

    Creating Blockchain class

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ from urllib.parse import urlparse
 import requests
 
 
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+        self.create_block(proof=1, previous_hash='0')
+        self.nodes = set()
